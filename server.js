e37d70f22431814455e3ee7a2da7981884336706
Vansh Bilwal <bilwal.vansh2002@gmail.com>
2022-01-27 14:38:30 +0530

    Deploy chnages

--- server.js
+++ server.js
@@ -34,4 +34,6 @@ if (process.env.NODE_ENV === 'production') {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server${process.env.NODE_ENV} started at PORT: ${PORT}`)
+);
